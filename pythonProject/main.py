3493609c450c55adde1cdbe4c9d7d397ba4ad086
Ezlestial <purpleplumd@gmail.com>
2024-01-19 23:21:16 -0500

    Lab 1

---
+++ pythonProject/main.py
@@ -0,0 +1,8 @@
+print("Zee Pearson(he/him)")
+print("This is his second year at OCC, he plans to switch to LTU next year for a bachelor's degree.")
+print("He enjoys video games such as Sekiro, Red Dead Redemption 2, Bloodborne and Dark Souls.")
+
+print("Perseus(he/they)")
+print("My name is Perseus and this is my second year at OCC, I plan to switch to a computer science program.")
+print("Outside of class I enjoy video games, reading and writing.")
+print("A fun fact about me is that I am learning Spanish.")
